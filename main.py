080310e0a5af1132aec1ef8a5fa9eaa0c7209cbd
Raúl Santoveña <raulsg619@gmail.com>
2022-07-06 13:24:43 -0400

    Add extended comments to the code

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+# Comments guidelines:
+
+##### [HEADING 1] #####
+### [HEADING 2] ###
+# Regular comment
+# END [HEADING 2] #
+#-- END [HEADING 1] --#
+
+# --------------------
+
+##### IMPORTS #####
 from bokeh.plotting import figure, curdoc 
 from bokeh.models import ColumnDataSource, ColorBar, FixedTicker, BasicTickFormatter, DataTable, TableColumn, HoverTool
 from bokeh.transform import linear_cmap, log_cmap
@@ -8,8 +19,9 @@ import pickle
 import os
 import pandas as pd
 import numpy as np
+#-- END IMPORTS --#
 
-
+##### LOAD DATA #####
 base_data_path = 'disentanglement_data'
 dataset_name = 'beta_vae_40'
 data_path = os.path.join(base_data_path, dataset_name)
@@ -22,13 +34,17 @@ wavelengths = np.load(os.path.join(data_path, 'wavelengths.npy'))
 
 # Dataframe
 dis_results_df = pd.read_pickle(os.path.join(data_path, 'dis_results.gz'))
+#-- END LOAD DATA --#
 
+##### PREPARE DATA #####
+### INVERSE TEMPERATURES ###
 _mock_labels = np.array([0.81681682, 0.55005005, 0.63313313, 0.23323323, 0.43343343,
        0.42842843, 1.        , 0.43343343, 0.63313313, 0.7997998 ,
        0.8998999 , 0.63313313, 0.43343343, 0.16666667, 0.36636637,
        0.53553554, 0.53353353, 0.2997998 , 0.43343343, 0.5       ,
        0.5       , 0.        , 0.        , 0.        , 0.        ,
-       0.1001001 ]) # Sacada de label_test
+       0.1001001 ])
+
 i_original_teff_list = list()
 i_new_teff_list = list()
 
@@ -45,24 +61,30 @@ for _, row_ in dis_results_df.iterrows():
     
 dis_results_df['i_original_teff'] = i_original_teff_list
 dis_results_df['i_new_teff'] = i_new_teff_list
+
 # Cast teffs to str
 dis_results_df['i_original_teff'] = dis_results_df['i_original_teff'].astype(str)
 dis_results_df['i_new_teff'] = dis_results_df['i_new_teff'].astype(str)
+# END INVERSE TEMPERATURES #
 
-
+# Group temperatures to calculate disentanglement results by temperature
 average_disentangled_results_df = dis_results_df.groupby(by=['i_original_teff', 'i_new_teff']).mean().reset_index()
+#-- END PREPARE DATA --#
 
-
+##### TOOLS AND TOOLTIPS #####
 GRID_TOOLS = ['tap', 'reset']
 SPECTRA_TOOLS = ['pan', 'box_zoom', 'reset']
 
-TOOLTIPS = [
+GRID_TOOLTIPS = [
     ("index", "$index"),
     ("original_teff", "@i_original_teff"),
     ("generated_teff", "@i_new_teff"),
     ("error", "@mse{(0.000)}")
 ]
+#-- END TOOLS AND TOOLTIPS --#
 
+##### BOKEH FIGURES #####
+### GRID FIGURE ###
 grid_df = average_disentangled_results_df.copy(deep=True)
 
 extended_source = ColumnDataSource(data=dis_results_df) # TODO I don't know if this is useful...
@@ -77,13 +99,11 @@ mapper = linear_cmap(field_name='mse', palette=palette, low=grid_df.mse.min(),
 x_range = np.unique(grid_df.i_original_teff)
 y_range = np.unique(grid_df.i_new_teff)
 
-
-# GRID
 p = figure(title="Disentanglement",
            x_range=x_range,
            y_range=y_range,
            tools=GRID_TOOLS,
-           tooltips=TOOLTIPS,
+           tooltips=GRID_TOOLTIPS,
            width=700, height=700)
 
 
@@ -101,7 +121,7 @@ p.axis.major_label_text_font_size = "10px"
 p.xaxis.axis_label = 'Original Teff'
 p.yaxis.axis_label = 'Generated Teff'
 
-
+# Color Bar
 n_ticks = 10  # how many ticks do you want?
 ticks = np.linspace(grid_df.mse.min(), grid_df.mse.max(), n_ticks)  # round to desired precision 
 color_ticks = FixedTicker(ticks=ticks)
@@ -110,9 +130,9 @@ color_bar = ColorBar(color_mapper=mapper['transform'],
                      ticker=color_ticks,
                      formatter=BasicTickFormatter(precision=2, use_scientific=False))
 p.add_layout(color_bar, 'right')
+# END GRID FIGURE #
 
-
-# DATATABLE
+### DATATABLE FIGURE ###
 table_source = ColumnDataSource(data=dict({
     'object_id': np.array([]),
     'original_teff': np.array([]),
@@ -127,9 +147,9 @@ table_columns = [
         TableColumn(field="mse", title="MSE"),
     ]
 data_table = DataTable(source=table_source, columns=table_columns) #, width=400, height=280)
+# END DATATABLE FIGURE #
 
-# SPECTRA COMPARISON FIGURE
-
+### SPECTRA COMPARISON FIGURE ###
 spectra_fig = figure(title="Spectra Comparison",
                      tools=SPECTRA_TOOLS,
                     )
@@ -152,18 +172,18 @@ spectra_fig.line(x="waves", y="spectra", source=orig_spectrum_source,
 spectra_fig.line(x="waves", y="spectra", source=new_spectrum_source, 
                  line_color='orange',
                  legend_label='New spectrum')
+# END SPECTRA COMPARISON FIGURE #
 
-####
-
-# LAYOUT CONFIGURATION
+### LAYOUT CONFIGURATION ###
 layout = row(p, 
          column(data_table, spectra_fig, sizing_mode='stretch_both'), 
          #data_table,
          sizing_mode='stretch_both')
+# END LAYOUT CONFIGURATION #
+#-- END BOKEH FIGURES --#
 
 
-
-### CALLBACKS
+##### CALLBACKS #####
 def update_table(attr, old, new):
     
     # If there is any cell selected we empty the datatable
@@ -246,5 +266,6 @@ def update_spectra(attr, old, new):
 source.selected.on_change('indices', update_table)
 
 table_source.selected.on_change('indices', update_spectra)
+#-- END CALLBACKS --#
 
 curdoc().add_root(layout)
